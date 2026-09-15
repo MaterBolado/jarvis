5eea5f5807c68f3a2a35a844dfc21af597fbc7e5
MaterBolado <miguelmanuelgarts@gmail.com>
2026-09-15 21:50:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -177,10 +177,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  // Only owner can use admin commands
-  if (interaction.commandName === "setpersonality" && interaction.user.id !== OWNER_ID) {
-    return interaction.reply("Only my creator can change my personality.");
-  }
+
 
   // CREATE EVENT
   if (interaction.commandName === "createevent") {
